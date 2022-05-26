69ebeb292c66c15b7bfcd009b91125980b315f32
Andre Hehn <a.hehn@web.de>
2022-05-26 21:53:22 +0200

    JSON defined

---
+++ script.js
@@ -0,0 +1,26 @@
+let carddeck = [
+    {
+        'question': "Inside which HTML element do we put the JavaScript?",
+        'answers': ["<java>", "<theScript>", "<script>", "<javascript>"],
+        'posRightAnswer': 2
+    },
+    {
+        'question': "How does a FOR loop start?",
+        'answers': ["for (let i=0 ; i<10 ; i++", "for i<10", "for (i=0 ; i<10) ", "for (let i =0 ; i++)"],
+        'posRightAnswer': 0
+    },
+    {
+        'question': "Which event occurs when the user clicks on an HTML element?",
+        'answers': ["onmouseover", "onchange", "onclick", "onmouseclick"],
+        'posRightAnswer': 2
+    },
+    {
+        'question': "Which operator is used to assign a value to a variable?",
+        'answers': ["x", "-", "+", "="],
+        'posRightAnswer': 3
+    }, {
+        'question': "What is the correct way to write a JavaScript array?",
+        'answers': ["var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", "var colors = 'red', 'green', 'blue'", "var colors = ['red', 'green', 'blue']  ", "var colors = (1:'red', 2:'green', 3:'blue')"],
+        'posRightAnswer': 2
+    },
+];
